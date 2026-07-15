e2a28843bc510bf37629f1ee8964fe6381fac3a1
Jenish J. Radadiya <jenish@sj.local>
2026-07-15 12:32:24 +0530

    Completed homepage and integrated EmailJS contact form

---
+++ js/contact.js
@@ -0,0 +1,53 @@
+// ===============================
+// EMAILJS INIT
+// ===============================
+
+emailjs.init({
+    publicKey: "pn-LVDvRW6geg-tAg",
+});
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const submitBtn = contactForm.querySelector("button");
+
+    const originalText = submitBtn.innerHTML;
+
+    submitBtn.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> Sending...';
+
+    submitBtn.disabled = true;
+
+    emailjs.sendForm(
+        "service_vi9futv",
+        "template_gjmp0ms",
+        contactForm
+    )
+
+    .then(() => {
+
+        contactForm.reset();
+
+        window.location.href = "thankyou.html";
+
+    })
+
+    .catch((error) => {
+
+        console.error(error);
+
+        alert("❌ Failed to send inquiry. Please try again.");
+
+    })
+
+    .finally(() => {
+
+        submitBtn.innerHTML = originalText;
+
+        submitBtn.disabled = false;
+
+    });
+
+});
